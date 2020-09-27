e6dc5ea713938ef6810ac2e83843a9d53df9a2d7
koyeary <katyeary@gmail.com>
2020-09-27 16:14:25 -0700

    event listener functional

--- assets/index.js
+++ assets/index.js
@@ -1,16 +1,21 @@
 $(document).ready(function () {
 
-  //setInterval()
   //Initialize material components
   $('.collapsible').collapsible();
+  
+  //Update the header and color coding every 10 seconds
+  setClock();
+  codeTime();
   setInterval(codeTime, 6000);
   
 
   function codeTime() {
-    //Get the current hour
+    //start the clock here so that clock and timecode are updated at the same interval
     setClock();
+
+    //Find the current time
     var currentHour = moment().hours();
-    console.log(currentHour);
+    /* find the current minute to set for alert toast */ 
 
     $('li').each( function() {
       var calendarHour = parseInt($(this).attr('value'));
@@ -28,16 +33,27 @@ $(document).ready(function () {
       var currentTime = moment().format('LLLL');
       $("h2").text(currentTime);
     }
- /*  function renderTime(time) {
-    localStorage.setItem(time); */
 
+   //function renderTime(time) {
+    //localStorage.setItem(time); 
 
+     $('.btn').on('click', function() {
+      //var description = this.previousElementSibling;
+      var time = this.id;
+      var text = $(`#description-${time}`).val();
+      console.log(text);
+      
+      console.log(time);
+      //Focuses on description input
+    }); 
+  
+/*     $(this.id).focusin(function () {
+
+    }) */
+    //get event form input and render event name to header
     //retrieve description from DOM (will be children or siblings)
     //$get HTML id, val, get information from storage
 
-  
-
-  codeTime();
 
 
 });
