7e69f600d98320b1f62ad0fb2c859b0c04fed7d4
koyeary <katyeary@gmail.com>
2020-09-28 17:05:04 -0700

    add badges

--- assets/index.js
+++ assets/index.js
@@ -1,77 +1,79 @@
 $(document).ready(function () {
-
   //Initialize material components
-  $('.collapsible').collapsible();
-  
-  //Update the header and color coding every 10 seconds
+  M.AutoInit();
+
+  //Initialize the calendar
   setClock();
+  loadCalendar();
   codeTime();
-  setInterval(codeTime, 6000);
-  
+
+  //Set the interval to upddate clock and corresponding color codes
+  setInterval(codeTime, 30000);
+
 
   function codeTime() {
-    //start the clock here so that clock and timecode are updated at the same interval
+    //Set the clock and timecode to update at the same interval
     setClock();
-    loadCalendar();
     //Find the current time
     var currentHour = moment().hours();
-    /* find the current minute to set for alert toast */ 
+    //if (currentHour > 18) {localStorage.clear()};
 
-    $('li').each( function() {
+    $('li').each(function () {
       var calendarHour = parseInt($(this).attr('value'));
-       if (currentHour > calendarHour) {
-        $(`#header-${calendarHour}`).addClass('teal lighten-4');
-      } else if (currentHour < calendarHour) {
-        $(`#header-${calendarHour}`).addClass('teal');
+      if (currentHour > calendarHour) {
+        $(`#header-${calendarHour}`).addClass('teal lighten-4'); //Event is in the past
+      } else if (currentHour < calendarHour) { 
+        $(`#header-${calendarHour}`).addClass('teal'); //Event is in the future
       } else if (currentHour === calendarHour) {
-      } 
+        console.log('now');
+      }
     });
   }
 
-    //Render the current date and time in the header
-    function setClock() {
-      var currentTime = moment().format('LLLL');
-      $("h2").text(currentTime);
-    }
+  //Render the current date and time in the header
+  function setClock() {
+    var currentTime = moment().format('LLLL');
+    $("h3").text(currentTime);
+  }
 
-   //function renderTime(time) {
-    //localStorage.setItem(time); 
+  //Save input fields to local storage
+  $('.btn').on('click', function () {
+    var time = this.id;
+    var eventName = $(`#name-${time}`).val();
+    var details = $(`#details-${time}`).val();
 
-//save input fields to local storage
-     $('.btn').on('click', function() {
-      var time = this.id;
-      var details = $(`#details-${time}`).val();
-      var eventName = $(`#name-${time}`).val();
-      
-      localStorage.setItem(`name-${time}`, eventName);
-      localStorage.setItem(`details-${time}`, details);
-      loadCalendar();
-      //Reload page and populate headers
-    }); 
-  
-     function loadCalendar() {
-      var id = 8;
-      for (let i = 0; i < 11; i++) {
-        var savedName = localStorage.getItem(`name-${id}`);
-        var savedDetails = localStorage.getItem(`details-${id}`)
-        console.log(savedDetails);
-        if (localStorage.getItem(`name-${id}`) === null) {
-          console.log('null');
-        } else {
-        $(`#header-${id}`).html(`${id}:00 ${savedName}`);  
-        $(`#body-${id}`).html(`${savedDetails}`);
-        }
-        id += 1;
+    localStorage.setItem(`name-${time}`, eventName);
+    localStorage.setItem(`details-${time}`, details);
+    //localStorage.setItem(`submit-date`, moment().format('ll'));
+    loadCalendar(); //Reload page and populate headers
+  });
+
+  function loadCalendar() {
+    var id = 8; //the HTML 'id' is the hour of the current key being rendered
+    for (let i = 0; i < 11; i++) {
+      var savedName = localStorage.getItem(`name-${id}`);
+      var savedDetails = localStorage.getItem(`details-${id}`);
+
+      if (localStorage.getItem(`name-${id}`) === null) {
+        console.log('null');
+      } else {
+        $(`#badge-${id}`).html(`${savedName}`);
+        //because the collapsible menu doesn't accomodate internal divs, icons have to be re-rendered.
+        $(`#body-${id}`).html(`<h5>${savedName}</h5><p>${savedDetails}</p>`);
       }
+      id += 1; //When event name and details are rendered, set the id to the next hour
     }
-    });  
-    
-/*     $(this.id).focusin(function () {
+  }  
 
-    }) */
-    //get event form input and render event name to header
-    //retrieve details from DOM (will be children or siblings)
-    //$get HTML id, val, get information from storage
+  //Set the calendar to expire after one day
+  /* function resetCalendar() {
+    var expire = moment().endOf('day').fromNow();
+    
+  } */
+
+});
+
+
 
 
 
