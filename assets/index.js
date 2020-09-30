b244bc51757e3a9bfa4b970aa906a21ca3e59873
koyeary <katyeary@gmail.com>
2020-09-29 17:48:02 -0700

    individual resets work

--- assets/index.js
+++ assets/index.js
@@ -5,48 +5,79 @@ $(document).ready(function () {
   //Initialize the calendar
   setClock();
   loadCalendar();
-  codeTime();
+  codeid();
 
   //Set the interval to upddate clock and corresponding color codes
-  setInterval(codeTime, 30000);
+  setInterval(codeid, 30000);
 
-  function codeTime() {
-    //Set the clock and timecode to update at the same interval
+  function codeid() {
+    //Set the clock and idcode to update at the same interval
     setClock();
-    //Find the current time
+    //Find the current id
     var currentHour = moment().hours();
-    //if (currentHour > 18) {localStorage.clear()};
 
     $('li').each(function () {
       var calendarHour = parseInt($(this).attr('value'));
       if (currentHour > calendarHour) {
         $(`#header-${calendarHour}`).addClass('teal lighten-5'); //Event is in the past
-        $(`#badge-${calendarHour}`).addClass('black-text');
+        $(`#badge-${calendarHour}`).toggleClass('black-text');
       } else if (currentHour < calendarHour) {
         $(`#header-${calendarHour}`).addClass('teal'); //Event is in the future
-        $(`#badge-${calendarHour}`).addClass('white-text');
+        $(`#badge-${calendarHour}`).toggleClass('white-text');
       } else if (currentHour === calendarHour) {
-        $(`#badge-${calendarHour}`).addClass('black-text');
+        $(`#header-${calendarHour}`).addClass('white');
+        $(`#badge-${calendarHour}`).toggleClass('black-text');
       }
     });
   }
 
-  //Render the current date and time in the header
+  //Render the current date and id in the header
   function setClock() {
-    var currentTime = moment().format('LLLL');
-    $("h3").text(currentTime);
+    var currentid = moment().format('LLLL');
+    $("h3").text(currentid);
   }
 
   //Save input fields to local storage
   $('.btn').on('click', function () {
-    var time = this.id;
-    var eventName = $(`#name-${time}`).val();
-    var details = $(`#details-${time}`).val();
+    var id = this.id;
+    var eventName = $(`#name-${id}`).val();
+    var details = $(`#details-${id}`).val();
 
-    localStorage.setItem(`name-${time}`, eventName);
-    localStorage.setItem(`details-${time}`, details);
+    localStorage.setItem(`name-${id}`, eventName);
+    localStorage.setItem(`details-${id}`, details);
     loadCalendar(); //Reload page and populate headers
   });
+
+  //Manually reset the calendar
+  $('.btn-large').on('click', function() {
+    localStorage.clear();
+    localStorage.setItem('expiration', now);
+    location.reload();
+  });
+
+  //Manually reset a single event
+  $('.reset').on('click', function() {
+    var id = this.id;
+    localStorage.removeItem(`name-${id}`, this.value)
+    localStorage.removeItem(`details-${id}`, this.value)
+    location.reload();
+  }); 
+
+  //Daily automated reset. If Mon (1) is not the same day as the expiration in local storage, reset the calendar.
+  function resetCalendar() {
+    var now = new Date().getDay();
+    var expiration = localStorage.getItem('expiration');
+
+    if (expiration === now) {
+      console.log(expiration);
+    } else if (expiration != now) {
+      localStorage.clear();
+      localStorage.setItem('expiration', now)
+    }
+  }
+
+
+
 
   function loadCalendar() {
     resetCalendar();
@@ -59,26 +90,14 @@ $(document).ready(function () {
         console.log('null');
       } else {
         $(`#badge-${id}`).html(`${savedName}`);
-        //because the collapsible menu doesn't accomodate internal divs, icons have to be re-rendered.
-        $(`#body-${id}`).html(`<h5>${savedName}</h5><blockquote>${savedDetails}</blockquote>`);
+        //reset card panel to event details with an option to reset and write a new event
+        $(`#body-${id}`).html(`<h5>${savedName}</h5><blockquote>${savedDetails}</blockquote><div class='card-action center-align'><button class='btn waves-effect waves-light reset' id='${id}'>Reset</button></div>`);
       }
       id += 1; //When event name and details are rendered, set the id to the next hour
     }
   }
 
-  //Refresh the calendar every day. If Mon (1) is not the same day as the expiration (2, etc...), it's time to reset the calendar.
-  function resetCalendar() {
-  var now = new Date().getDay();
-  var expiration = localStorage.getItem('expiration');
 
-  if (expiration === now) {
-    console.log(expiration);
-  } else if (expiration != now) {
-    localStorage.clear();
-    localStorage.setItem('expiration', now)
-  } 
-}
-  
 });
 
 
